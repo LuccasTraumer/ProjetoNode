8f22f17353c98b1e282be76e87e22b16df066704
LuccasTraumer <lucas.jeesus@hotmail.com>
2019-12-03 14:29:22 -0300

    Só o que falta é o que nao sei fazer

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ const conexao = require('./config/custom-mssql');
 app.get('/',function (req,resp) {
     var requisicao = new conexao.Request();
 
-    requisicao.query("select top 4 * from dbo.PRODUTOS",
+    requisicao.query("select top 9 * from dbo.PRODUTOS",
         function (codErro,RecordSet) {
         if(codErro)
             console.log("Erro no Banco de Dados: "+codErro);
